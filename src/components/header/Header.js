4d66d7c943aa06670e7883dc415fd053f886ee04
Anish-Udupa <udupa.anish@gmail.com>
2023-08-12 15:56:18 +0530

    Added dark mode

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
+import { useState } from "react";
 
 function Header(){
     const navigate = useNavigate();
@@ -9,12 +10,27 @@ function Header(){
         navigate("/");
     }
 
+    const [ isDark, setIsDark ] = useState(false);
+
+    const toggleDarkMode = () => {
+        if(isDark) {
+            // Switch to light mode
+            document.body.classList.remove("dark");
+            setIsDark(false);
+        }
+        else {
+            // Switch to dark mode
+            document.body.classList.add("dark");
+            setIsDark(true);
+        }
+    }
+
     return(
         <div id="header-container">
             <p id="header-title" onClick={onTitleClick}>Where in the world?</p>
-            <div id="header-dark-mood-container">
-                <FontAwesomeIcon icon={faMoon} size="lg"  />
-                <p id="dark-mode-text">Dark Mode</p>
+            <div id="header-dark-mood-container"  onClick={toggleDarkMode}>
+                {isDark ? <FontAwesomeIcon icon={faSun} size="lg" /> : <FontAwesomeIcon icon={faMoon} size="lg"  />}
+                <p id="dark-mode-text">{isDark ? "Light Mode" : "Dark Mode"}</p>
             </div>
         </div>
     )
