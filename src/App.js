6b7c8e290315b20bb44cfb83489e5c418f2ec4b4
Anish-Udupa <udupa.anish@gmail.com>
2023-08-13 11:11:28 +0530

    Updated public and added hashrouter

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import './App.css';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import { Route, Routes } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import CountryFull from './components/country-full/CountryFull';
 
 function App() {
   return (
     <div id="app">
-      <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/country" element={<CountryFull />} />
         </Routes>
-      </BrowserRouter>
-      
     </div>
   );
 }
