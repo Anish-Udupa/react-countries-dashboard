fcb0e0ec2f71da9fef27006b1458351374518d1a
Anish-Udupa <udupa.anish@gmail.com>
2023-08-20 14:17:35 +0530

    Fixed error

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/country" element={<CountryFull />} />
-          <Route path="/abc" element={<Error1 />} />
         </Routes>
     </div>
   );
